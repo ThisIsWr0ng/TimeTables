1b26e25a57aaaeae8d4dec4fe7898f36c61b59b3
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-03-07 18:38:04 +0000

    Added multiple constraints

--- SQL Tables.sql
+++ SQL Tables.sql
@@ -3,39 +3,40 @@
 --Version 1.5.3
 CREATE TABLE `Users` (
   `Id` VARCHAR(50),
-  `First_Name` VARCHAR(50),
-  `Surname` VARCHAR(50),
+  `First_Name` VARCHAR(50) NOT NULL,
+  `Surname` VARCHAR(50) NOT NULL,
   `Title` VARCHAR(10),
-  `Email` VARCHAR(50),
-  `Telephone` VARCHAR(50),
+  `Email` VARCHAR(50) NOT NULL,
+  `Telephone` VARCHAR(50) NOT NULL,
   `Next_Of_Kin` VARCHAR(50),
-  `Street_Number` VARCHAR(5),
+  `Street_Number` VARCHAR(5) NOT NULL,
   `Street_Name` VARCHAR(50),
-  `Postcode` VARCHAR(8),
+  `Postcode` VARCHAR(8) NOT NULL,
   PRIMARY KEY (`Id`)
 );
 
 CREATE TABLE `Requests` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `User_Id` VARCHAR(50),
-  `Type` VARCHAR(50),
+  `Type` VARCHAR(50) NOT NULL,
   `Description` VARCHAR(200),
   PRIMARY KEY (`Id`),
-  FOREIGN KEY (`User_Id`) REFERENCES `Users`(`Id`)
+  FOREIGN KEY (`User_Id`) REFERENCES `Users`(`Id`),
+  INDEX (Type)
 );
 
 CREATE TABLE `Modules` (
   `Id` VARCHAR(50),
-  `Name` VARCHAR(50),
+  `Name` VARCHAR(50) NOT NULL,
   `Description` VARCHAR(200),
   PRIMARY KEY (`Id`)
 );
 
 CREATE TABLE `Buildings` (
   `Name` VARCHAR(50),
-  `Street_Number` VARCHAR(5),
+  `Street_Number` VARCHAR(5) NOT NULL,
   `Street_Name` VARCHAR(50),
-  `Postcode` VARCHAR(8),
+  `Postcode` VARCHAR(8) NOT NULL,
   PRIMARY KEY (`Name`)
 );
 
@@ -51,48 +52,49 @@ CREATE TABLE `Rooms` (
 );
 
 CREATE TABLE `Events` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Module` VARCHAR(50),
   `Room` VARCHAR(10),
   `Type` VARCHAR(50),
-  `Day_Of_Week` CHAR(1),
-  `Time_From` TIME,
-  `Time_To` TIME,
+  `Day_Of_Week` CHAR(1) NOT NULL,
+  `Time_From` TIME NOT NULL,
+  `Time_To` TIME NOT NULL,
   `Description` VARCHAR(200),
   PRIMARY KEY (`Id`),
   FOREIGN KEY (`Module`) REFERENCES `Modules`(`Id`),
-  FOREIGN KEY (`Room`) REFERENCES `Rooms`(`Number`)
+  FOREIGN KEY (`Room`) REFERENCES `Rooms`(`Number`),
+  INDEX (Module)
 );
 
 CREATE TABLE `Departments` (
   `Name` VARCHAR(50),
-  `Managed_By` VARCHAR(50),
+  `Managed_By` VARCHAR(50) NOT NULL,
   `Description` VARCHAR(200),
   PRIMARY KEY (`Name`)
 );
 
 CREATE TABLE `Programmes` (
   `Id` VARCHAR(50),
-  `Name` VARCHAR(50),
+  `Name` VARCHAR(50) NOT NULL,
   `Department` VARCHAR(50),
-  `Level` CHAR(1),
-  `Start_Date` DATE,
-  `End_Date` DATE,
+  `Level` CHAR(1) NOT NULL,
+  `Start_Date` DATE NOT NULL CHECK (Start_Date LIKE '--/--/----'),
+  `End_Date` DATE NOT NULL CHECK (End_Date LIKE '--/--/----'),
   `Description` VARCHAR(200),
   PRIMARY KEY (`Id`),
   FOREIGN KEY (`Department`) REFERENCES `Departments`(`Name`)
 );
 
 CREATE TABLE `Semesters` (
-  `Id` VARCHAR(50),
-  `Start_Date` DATE,
-  `End_Date` DATE,
+  `Id` VARCHAR(50) AUTO_INCREMENT,
+  `Start_Date` DATE NOT NULL CHECK (Start_Date LIKE '--/--/----'),
+  `End_Date` DATE NOT NULL CHECK (End_Date LIKE '--/--/----'),
   `Name` VARCHAR(50),
   PRIMARY KEY (`Id`)
 );
 
 CREATE TABLE `Module_Assignment` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Programme` VARCHAR(50),
   `Module` VARCHAR(50),
   `Semester` VARCHAR(50),
@@ -103,7 +105,7 @@ CREATE TABLE `Module_Assignment` (
 );
 
 CREATE TABLE `Lecturers_Assignment` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Lecturer` VARCHAR(50),
   `Module` VARCHAR(50),
   PRIMARY KEY (`Id`),
@@ -113,32 +115,32 @@ CREATE TABLE `Lecturers_Assignment` (
 
 CREATE TABLE `Logins` (
   `Username` VARCHAR(30),
-  `Password` VARCHAR(50),
+  `Password` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`Username`)
 );
 
 CREATE TABLE `Student_Enrollment` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Student` VARCHAR(50),
   `Programme` VARCHAR(50),
-  `Level` CHAR(1),
-  `Date_Enrolled` DATE,
-  `Date_Finished` DATE,
+  `Level` CHAR(1) NOT NULL,
+  `Date_Enrolled` DATE CHECK (Date_Enrolled LIKE '--/--/----'),
+  `Date_Finished` DATE CHECK (Date_Finished LIKE '--/--/----'),
   PRIMARY KEY (`Id`),
   FOREIGN KEY (`Student`) REFERENCES `Users`(`Id`),
   FOREIGN KEY (`Programme`) REFERENCES `Programmes`(`Id`)
 );
 
 CREATE TABLE `Roles` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Name` VARCHAR(50),
-  `Access_Level` CHAR(1),
+  `Access_Level` CHAR(1) NOT NULL,
   `Description` VARCHAR(200),
   PRIMARY KEY (`Id`)
 );
 
 CREATE TABLE `Role_Assignment` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Role` VARCHAR(50),
   `User` VARCHAR(50),
   PRIMARY KEY (`Id`),
@@ -147,7 +149,7 @@ CREATE TABLE `Role_Assignment` (
 );
 
 CREATE TABLE `Student_Timetable` (
-  `Id` VARCHAR(50),
+  `Id` VARCHAR(50) AUTO_INCREMENT,
   `Student` VARCHAR(50),
   `Module` VARCHAR(50),
   PRIMARY KEY (`Id`),
@@ -156,18 +158,18 @@ CREATE TABLE `Student_Timetable` (
 );
 
 CREATE TABLE `Holidays` (
-  `Id` VARCHAR(50),
-  `Date_From` DATE,
-  `Date_To` DATE,
+  `Id` VARCHAR(50) AUTO_INCREMENT,
+  `Date_From` DATE NOT NULL CHECK (Date_From LIKE '--/--/----'),
+  `Date_To` DATE NOT NULL CHECK (Date_To LIKE '--/--/----'),
   `Description` VARCHAR(200),
   PRIMARY KEY (`Id`)
 );
 
 CREATE TABLE `Settings` (
   `User` VARCHAR(50),
-  `Color_Scheme` CHAR(1),
-  `Notifications` CHAR(1),
-  `Default_View` CHAR(1),
+  `Color_Scheme` CHAR(1) DEFAULT 1,
+  `Notifications` CHAR(1) DEFAULT 1,
+  `Default_View` CHAR(1) DEFAULT 1,
   PRIMARY KEY (`User`)
 );
 
