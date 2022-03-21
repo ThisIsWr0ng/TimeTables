f32e923066d3b8c38adf7bd3c254f55c2b039809
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-03-21 20:26:00 +0000

    blank page

--- Site/personal_timetable.php
+++ Site/personal_timetable.php
@@ -1,1 +1,12 @@
-
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Personal Timetable</title>
+  <meta name="description" content="Personal Timetable">
+  <link rel="stylesheet" href="Style/Basic.css">
+</head>
+<body>
+    
+</body>
+</html>
