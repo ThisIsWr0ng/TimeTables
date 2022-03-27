1ccca22e41c3820329d1c97b6548430c847405f8
PerskiSzah <101399862+PerskiSzah@users.noreply.github.com>
2022-03-27 22:41:33 +0100

    Update users-table.php

--- Login/users-table.php
+++ Login/users-table.php
@@ -10,19 +10,14 @@ $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
 /*
 Description of columns in the users table:
 id - unique user ID;
-registered_timestamp - date of registration in the form of a timestamp;
-username - username;
 email - user's email;
 password - user password;
-ip - IP from which the account was created;
 */
 
-$dbh->exec("CREATE TABLE IF NOT EXISTS `users` (
- `id` int(10) NOT NULL AUTO_INCREMENT,
- `registered_timestamp` int(10) NOT NULL,
- `username` varchar(30) NOT NULL,
- `email` varchar(50) NOT NULL,
- `password` varchar(255) NOT NULL,
- `ip` varchar(100) NOT NULL,
- PRIMARY KEY (`id`)
+CREATE TABLE `user` (
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`email` VARCHAR(255) NOT NULL,
+`password` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ENGINE=InnoDB AUTO_INCREMENT=1
  )");
