015a258fe8fd64b7de2bff8c2540199470ce6b0f
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-06 20:31:55 +0100

    document formatting

--- Site/SQL/SQL Tables and data.sql
+++ Site/SQL/SQL Tables and data.sql
@@ -72,7 +72,7 @@ CREATE TABLE `Rooms` (
   `Type` VARCHAR(50),
   `Equipment` VARCHAR(200),
   `Section` VARCHAR(50),
-  PRIMARY KEY (`Number`,`Building`),
+  PRIMARY KEY (`Number`, `Building`),
   FOREIGN KEY (`Building`) REFERENCES `Buildings`(`Name`)
 );
 CREATE TABLE `Events` (
@@ -185,95 +185,88 @@ CREATE TABLE `Settings` (
   `Default_View` CHAR(1) DEFAULT 1,
   PRIMARY KEY (`User`)
 );
-
 INSERT INTO users
 VALUES (
-        "S19005373",
-        "Dawid",
-        "Olesko",
-        "Mr",
-        "Male",
-        "dawidolesko@gmail.com",
-        "S19005373@mail.glyndwr.ac.uk",
-        "+4407923426783",
-        "Wife, +4407934532286",
-        "55",
-        "Sealand Street",
-        "CH52HM"
-    );
-
-INSERT INTO settings VALUES (
+    "S19005373",
+    "Dawid",
+    "Olesko",
+    "Mr",
+    "Male",
+    "dawidolesko@gmail.com",
+    "S19005373@mail.glyndwr.ac.uk",
+    "+4407923426783",
+    "Wife, +4407934532286",
+    "55",
+    "Sealand Street",
+    "CH52HM"
+  );
+INSERT INTO settings
+VALUES (
     "S19005373",
     "1",
     "1",
     "1"
-     );
-INSERT INTO logins VALUES (
-    "S19005373",
-    "1234"
-);
-INSERT INTO logins VALUES (
-    "admin",
-    "admin"
-);
-INSERT INTO logins VALUES (
-    "root",
-    "mysql"
-);
-
-INSERT INTO roles VALUES (
+  );
+INSERT INTO logins
+VALUES ("S19005373", "1234");
+INSERT INTO logins
+VALUES ("admin", "admin");
+INSERT INTO logins
+VALUES ("root", "mysql");
+INSERT INTO roles
+VALUES (
     NULL,
     "Root",
     0,
     "Root access, full control over servers and database"
-);
-INSERT INTO roles VALUES (
+  );
+INSERT INTO roles
+VALUES (
     NULL,
     "Admin",
     1,
     "Admin, access to database"
-);
-INSERT INTO roles VALUES (
+  );
+INSERT INTO roles
+VALUES (
     NULL,
     "IT Technician",
     2,
     "Maintanance in networks and university computers"
-);
-INSERT INTO roles VALUES (
+  );
+INSERT INTO roles
+VALUES (
     NULL,
     "Lecturer",
     3,
     "User, requests allowed"
-);
-INSERT INTO roles VALUES (
+  );
+INSERT INTO roles
+VALUES (
     NULL,
     "Undergraduate Student",
     4,
     "User, restricted access"
-);
-
-INSERT INTO role_assignment VALUES (
-    NULL,
-    5,
-    "S19005373"
-);
-
-INSERT INTO holidays VALUES (
+  );
+INSERT INTO role_assignment
+VALUES (NULL, 5, "S19005373");
+INSERT INTO holidays
+VALUES (
     NULL,
     "Easter Break",
     "2022-04-11",
     "2022-04-24",
     "Two weeks off for easter holidays in semester 2"
-);
-
-INSERT INTO departments VALUES (
+  );
+INSERT INTO departments
+VALUES (
     "Computing",
     "COM",
     "John Doe",
     ""
-);
-
-INSERT INTO programmes Values (
+  );
+INSERT INTO programmes
+Values (
     NULL,
     "BSc (Hons) Computer Science - Year 2 - Full Time",
     "Computing",
@@ -281,8 +274,9 @@ INSERT INTO programmes Values (
     "2021-09-20",
     "2022-07-01",
     "Computer Science"
-);
-INSERT INTO programmes Values (
+  );
+INSERT INTO programmes
+Values (
     NULL,
     "BSc (Hons) Computing - Year 1 - Full Time",
     "Computing",
@@ -290,141 +284,118 @@ INSERT INTO programmes Values (
     "2021-09-20",
     "2022-07-01",
     "Computing Programme"
-);
-
-INSERT INTO student_enrollment VALUES (
+  );
+INSERT INTO student_enrollment
+VALUES (
     NULL,
     "S19005373",
     1,
     NOW(),
     NULL
-);
-
-INSERT INTO modules VALUES (
+  );
+INSERT INTO modules
+VALUES (
     "COM539",
     "Data Structures and Algorithms",
     "Maths module",
     "https://moodle.glyndwr.ac.uk/course/view.php?id=35178"
-);
-INSERT INTO modules VALUES (
+  );
+INSERT INTO modules
+VALUES (
     "COM540",
     "Databases and Web-based Information Systems",
     "",
     "https://moodle.glyndwr.ac.uk/course/view.php?id=35179"
-);
-INSERT INTO modules VALUES (
+  );
+INSERT INTO modules
+VALUES (
     "COM553",
     "Group Project",
     "",
     "https://moodle.glyndwr.ac.uk/course/view.php?id=39159"
-);
-INSERT INTO modules VALUES (
+  );
+INSERT INTO modules
+VALUES (
     "COM545",
     "Responsible Computing",
     "",
     "https://moodle.glyndwr.ac.uk/course/view.php?id=35184"
-);
-
-INSERT INTO buildings VALUES (
+  );
+INSERT INTO buildings
+VALUES (
     "Main Building",
     "1",
     "Mold Road",
     "LL11 2AW"
-);
-
-INSERT INTO rooms VALUES (
+  );
+INSERT INTO rooms
+VALUES (
     "B119",
     "Main Building",
     25,
     "PC Room",
     "Projector, 20 pc's",
     "B"
-);
-INSERT INTO rooms VALUES (
+  );
+INSERT INTO rooms
+VALUES (
     "B117",
     "Main Building",
     25,
     "PC Room",
     "Projector, 20 pc's",
     "B"
-);
-INSERT INTO rooms VALUES (
+  );
+INSERT INTO rooms
+VALUES (
     "C124",
     "Main Building",
     60,
     "Lecture Room",
     "Projector",
     "C"
-);
-INSERT INTO rooms VALUES (
+  );
+INSERT INTO rooms
+VALUES (
     "B118",
     "Main Building",
     60,
     "Lecture Room",
     "Projector",
     "B"
-);
-
-INSERT INTO semesters VALUES (
+  );
+INSERT INTO semesters
+VALUES (
     NULL,
     "2021-09-20",
     "2022-01-28",
     "Semester One"
-);
-INSERT INTO semesters VALUES (
+  );
+INSERT INTO semesters
+VALUES (
     NULL,
     "2022-01-31",
     "2022-07-01",
     "Semester two"
-);
-
-INSERT INTO module_assignment VALUES (
-    NULL,
-    1,
-    "COM539",
-    1
-);
-INSERT INTO module_assignment VALUES (
-    NULL,
-    1,
-    "COM540",
-    2
-);
-INSERT INTO module_assignment VALUES (
-    NULL,
-    1,
-    "COM553",
-    2
-);
-INSERT INTO module_assignment VALUES (
-    NULL,
-    1,
-    "COM545",
-    2
-);
-
-INSERT INTO Student_Timetable VALUES (
-    NULL,
-    "S19005373",
-    1
-);
-INSERT INTO Student_Timetable VALUES (
-    NULL,
-    "S19005373",
-    2
-);
-INSERT INTO Student_Timetable VALUES (
-    NULL,
-    "S19005373",
-    3
-);
-INSERT INTO Student_Timetable VALUES (
-    NULL,
-    "S19005373",
-    4
-);
-
-INSERT INTO events VALUES (
+  );
+INSERT INTO module_assignment
+VALUES (NULL, 1, "COM539", 1);
+INSERT INTO module_assignment
+VALUES (NULL, 1, "COM540", 2);
+INSERT INTO module_assignment
+VALUES (NULL, 1, "COM553", 2);
+INSERT INTO module_assignment
+VALUES (NULL, 1, "COM545", 2);
+INSERT INTO Student_Timetable
+VALUES (NULL, "S19005373", 1);
+INSERT INTO Student_Timetable
+VALUES (NULL, "S19005373", 2);
+INSERT INTO Student_Timetable
+VALUES (NULL, "S19005373", 3);
+INSERT INTO Student_Timetable
+VALUES (NULL, "S19005373", 4);
+INSERT INTO events
+VALUES (
     NULL,
     "COM539",
     "B119",
@@ -433,8 +404,9 @@ INSERT INTO events VALUES (
     "09:00:00",
     "12:00:00",
     NULL
-);
-INSERT INTO events VALUES (
+  );
+INSERT INTO events
+VALUES (
     NULL,
     "COM540",
     "B117",
@@ -443,4 +415,24 @@ INSERT INTO events VALUES (
     "09:00:00",
     "12:00:00",
     NULL
-);
+  );
+INSERT INTO `events` (
+    `Id`,
+    `Module`,
+    `Room`,
+    `Type`,
+    `Date`,
+    `Time_From`,
+    `Time_To`,
+    `Description`
+  )
+VALUES (
+    NULL,
+    'COM545',
+    'C124',
+    'Lecture',
+    '2022-04-12',
+    '12:00:00',
+    '16:00:00',
+    NULL
+  );
--- Site/personal_timetable.php
+++ Site/personal_timetable.php
@@ -1,73 +1,83 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Personal Timetable</title>
-  <meta name="description" content="Personal Timetable">
-  <link rel="stylesheet" href="Style/Basic.css">
-  <link href="style/calendar.css" rel="stylesheet" type="text/css">
-  <script src="script/calendar.js"></script>
-  <?php  include 'php/conn.php'; ?>
-  <?php  include 'php/fetch_events.php'; ?>
+  <head>
+    <meta charset="utf-8" />
+    <title>Personal Timetable</title>
+    <meta name="description" content="Personal Timetable" />
+    <link rel="stylesheet" href="Style/Basic.css" />
+    <link href="style/calendar.css" rel="stylesheet" type="text/css" />
+    <script src="script/calendar.js"></script>
+    <?php  include 'php/conn.php'; ?>
+    <?php  include 'php/fetch_events.php'; ?>
+  </head>
+  <body>
+    <header>
+      <img
+        id="logo"
+        src="img/TimeTables-logos/TimeTables-logos_white_cropped.png"
+        alt="TimeTables Logo"
+      />
+    </header>
+    <main class="content">
+      <div id="cal-wrap">
+        <!-- (A) PERIOD SELECTOR -->
+        <div id="cal-date">
+          <select id="cal-mth"></select>
+          <select id="cal-yr"></select>
+        </div>
 
+        <!-- (B) CALENDAR -->
+        <div id="cal-container"></div>
 
-</head>
-<body>
-  <header><img id="logo" src="img/TimeTables-logos/TimeTables-logos_white_cropped.png" alt="TimeTables Logo"></header>
-  <main class="content">
-    
-  <div id="cal-wrap">
-      <!-- (A) PERIOD SELECTOR -->
-      <div id="cal-date">
-        <select id="cal-mth"></select>
-        <select id="cal-yr"></select>
+        <!-- (C) EVENT FORM -->
+        <div id="overlay">
+          <form id="cal-event" action="php/eventRequest.php" method="post">
+            <div id="evt-head"></div>
+            <input type="date" id="evt-date" name="date" />
+            <div id="evt-time"></div>
+            <div id="evt-room"></div>
+            <div id="request-fileds">
+              <label for="eName">Event name:</label>
+              <input type="text" id="eName" name="eName" required /><br />
+
+              <label for="tFrom">Time From:</label>
+              <input
+                type="time"
+                id="tFrom"
+                name="timeFrom"
+                min="12:00"
+                max="00:00"
+                required
+              />
+              <label for="tTo">Time To:</label>
+              <input
+                type="time"
+                id="tTo"
+                name="timeTo"
+                min="12:00"
+                max="00:00"
+              /><br />
+
+              <label for="evt-details">Event description:</label>
+            </div>
+
+            <textarea id="evt-details" name="details" readonly></textarea>
+            <input id="evt-close" type="button" value="Close" />
+            <input id="evt-request" type="button" value="Add Event" />
+            <input id="evt-moodle-page" type="button" value="Moodle Page" />
+            <input id="evt-next" type="button" value="Next -->" />
+          </form>
+        </div>
       </div>
 
-      <!-- (B) CALENDAR -->
-      <div id="cal-container"></div>
-
-      <!-- (C) EVENT FORM -->
-      <div id="overlay"><form id="cal-event" action="php/eventRequest.php" method="post">
-        <div id="evt-head"></div>
-        <input type="date" id="evt-date" name="date"/>
-        <div id="evt-time"></div>
-        <div id="evt-room"></div>
-        <div id="request-fileds">
-        
-  <label for="eName">Event name:</label>
-  <input type="text" id="eName" name="eName"required><br/>
-
-  <label for="tFrom">Time From:</label>
-  <input type="time" id="tFrom" name="timeFrom"
-       min="12:00" max="00:00" required>
-       <label for="tTo">Time To:</label>
-  <input type="time" id="tTo" name="timeTo"
-       min="12:00" max="00:00"><br/>
-
-  <label for="evt-details">Event description:</label>
-</div>
-
-        <textarea id="evt-details" name="details"readonly></textarea>
-        <input id="evt-close" type="button" value="Close"/>
-        <input id="evt-request" type="button" value="Add Event"/>
-        <input id="evt-moodle-page" type="button" value="Moodle Page"/>
-        <input id="evt-next" type="button" value="Next -->"/>
-</form>
-    </div></div>
-    
-   
-    <script>
+      <script>
         function loadContent(a, b){
             document.getElementById(a).innerHTML= b;
         }
             var dbData = JSON.parse( '<?php echo json_encode(fetchEvents('S19005373')) ?>' );//<-----------------Pass student number here
             console.log("Data Received:", dbData);
             window.addEventListener("load", drawCalendar(dbData));
-   
-
-        </script>
-
-  </main>
-   
-</body>
+      </script>
+    </main>
+  </body>
 </html>
