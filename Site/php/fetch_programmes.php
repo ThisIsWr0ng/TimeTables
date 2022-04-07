21bbced7f5e50558853d906b401f055d89313293
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-07 14:11:13 +0100

    Fetch programmes.php

---
+++ Site/php/fetch_programmes.php
@@ -0,0 +1,5 @@
+<?php 
+include_once conn.php;
+$sql ="SELECT * FROM programmes";
+
+?>
--- Site/timetable_finder.php
+++ Site/timetable_finder.php
@@ -1,1 +1,24 @@
 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Personal Timetable</title>
+    <meta name="description" content="Personal Timetable" />
+    <link rel="stylesheet" href="Style/Basic.css" />
+    <?php  include 'php/conn.php'; ?>
+    <?php  include 'php/fetch_programmes.php'; ?>
+  </head>
+  <body>
+    <header>
+      <img
+        id="logo"
+        src="img/TimeTables-logos/TimeTables-logos_white_cropped.png"
+        alt="TimeTables Logo"
+      />
+    </header>
+    <main class="content">
+
+    </main>
+  </body>
+</html>
