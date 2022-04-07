d68f764174c0b597955dae3756464754d0e907df
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-07 18:18:34 +0100

    First working timetable search

--- Site/php/search_timetables.php
+++ Site/php/search_timetables.php
@@ -1,21 +1,17 @@
-<?php 
-//$type =$_POST['type'];
-//$text =$_POST['text'];
+<?php
 
-//if($type == "programme"){
- //fetchProgrammes($text);
-//}
+include 'conn.php';
+$q= $_REQUEST["q"];
+$searchType = $_REQUEST["type"];
 
-
-
-    include 'conn.php';
-    $sql = "SELECT * FROM programmes";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      $array = null;
-        $i = 0;
-        while ($row = $result->fetch_assoc()) {
-          $array[$i] = array(
+$sql = "SELECT * FROM programmes";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $array = null;
+    $i = 0;
+    while ($row = $result->fetch_assoc()) {
+        $array[$i] = array(
+            'id' => $row["Id"],
             'name' => $row["Name"],
             'degree' => $row["Degree"],
             'department' => $row["Department"],
@@ -24,37 +20,45 @@
             'start_date' => $row["Start_Date"],
             'end_date' => $row["End_Date"],
             'description' => $row["Description"],
-          );
-          $i += 1;
-        }
-    } else {
-        echo "0 results";
+        );
+        $i += 1;
     }
-    //return $array;
-    $q = $_REQUEST["q"];
+} else {
+    echo "0 results";
+}
+ 
 
 $hint = "";
 
 // lookup all hints from array if $q is different from ""
-if ($q !== "") {
-  $q = strtolower($q);
-  $len=strlen($q);
-  echo "<table><tr><th>Degree</th><th>Name</th><th>Level</th><th>Type</th></tr>";
-  for ($i=0; $i < count($array); $i++) { 
-    if (stristr($q, substr($array[$i]['name'], 0, $len))) {
-       echo "<tr><td>{$array[$i]['degree']}</td><td>{$array[$i]['name']}</td><td>{$array[$i]['level']}</td><td>{$array[$i]['type']}</td></tr>";
-        /* $hint = [];
-        array_push($hint, $array[$i]);*/
-      /*if ($hint === "") {
-        $hint = $array[$i]['name'];
-      } else {
-        $hint .= ", $array[$i]['name']";
-      }*/
+echo "<table><tr><th>Degree</th><th>Name</th><th>Level</th><th>Type</th></tr>";
+if ($q == "alL" || $q == "") {
+    for ($i = 0; $i < count($array); $i++) {
+        $find = strtolower($array[$i]['name']);
+        if (strpos($find, $q) !== false) {
+            echo "<tr class=\"clickable-row\" onclick=\"window.location='timetable.php?id={$array[$i]['id']}'\">
+          <td>{$array[$i]['degree']}</td>
+          <td>{$array[$i]['name']}</td>
+          <td>{$array[$i]['level']}</td>
+          <td>{$array[$i]['type']}</td></tr>";
+        }
     }
-  }
+} else {
+    $q = strtolower($q);
+    $len = strlen($q);
+
+    for ($i = 0; $i < count($array); $i++) {
+        $find = strtolower($array[$i]['name']);
+        if (strpos($find, $q) !== false) {
+            echo "<tr class=\"clickable-row\" onclick=\"window.location='timetable.php?id={$array[$i]['id']}'\">
+          <td>{$array[$i]['degree']}</td>
+          <td>{$array[$i]['name']}</td>
+          <td>{$array[$i]['level']}</td>
+          <td>{$array[$i]['type']}</td></tr>";
+        }
+
+    }
 }
+echo "</table>";
 
 // Output "no suggestion" if no hint was found or output correct values
-
-
-?>
