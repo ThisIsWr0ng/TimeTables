aa54e1a3ea52b7a68c602f1993b77b36d55ef8ef
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-07 14:58:33 +0100

    reworked programmes table

--- Site/php/fetch_data.php
+++ Site/php/fetch_data.php
@@ -1,10 +1,12 @@
-<?php 
-function fetchEvents($student) {//Fetch event data based on user ID
+//Code snippets from: https://www.w3schools.com/
+<?php
+function fetchEvents($student)
+{ //Fetch event data based on user ID
     include 'conn.php';
     $sql = "SELECT `events`.*,`rooms`.`type` AS room_type,`modules`.`name`AS module_name,`modules`.`description` AS module_description, `modules`.`moodle_link`, `module_assignment`.`semester`, `programmes`.`name`AS programme_name, `semesters`.`start_date`, `semesters`.`end_date`
-    FROM `events` 
-      LEFT JOIN `modules` ON `events`.`Module` = `modules`.`Id` 
-      LEFT JOIN `module_assignment` ON `module_assignment`.`Module` = `modules`.`Id` 
+    FROM `events`
+      LEFT JOIN `modules` ON `events`.`Module` = `modules`.`Id`
+      LEFT JOIN `module_assignment` ON `module_assignment`.`Module` = `modules`.`Id`
       LEFT JOIN `programmes` ON `module_assignment`.`Programme` = `programmes`.`Id`
         LEFT JOIN `semesters` ON `module_assignment`.`Semester` = `semesters`.`Id`
         LEFT JOIN `rooms` ON `events`.`Room` = `rooms`.`Number`
@@ -12,41 +14,52 @@ function fetchEvents($student) {//Fetch event data based on user ID
         WHERE `student_enrollment`.`Student` = '$student';";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-      // output data of each row
-      $array = null;
-      $i = 0;
-      while($row = $result->fetch_assoc()) {//separate outputs with for loop?
-        $array[$i]= array(
-        'module' => $row["Module"],
-        'room' => $row["Room"],
-        'type' => $row["Type"],
-        'date' => $row["Date"],
-        'time_from' => $row["Time_From"],
-        'time_to' => $row["Time_To"],
-        'description' => $row["Description"],
-        'room_type' => $row["room_type"],
-        'module_name' => $row["module_name"],
-        'module_description' => $row["module_description"],
-        'moodle_link' => $row["moodle_link"],
-        'semester' => $row["semester"],
-        'programme_name' => $row["programme_name"],
-        'start_date' => $row["start_date"],
-        'end_date' => $row["end_date"],
-      );
-        $i += 1;
-      }
-     return $array;
-      
-    } else{
-      echo "Error Fetching from database";
+        $array = null;
+        $i = 0;
+        while ($row = $result->fetch_assoc()) { //separate outputs with for loop?
+            $array[$i] = array(
+                'module' => $row["Module"],
+                'room' => $row["Room"],
+                'type' => $row["Type"],
+                'date' => $row["Date"],
+                'time_from' => $row["Time_From"],
+                'time_to' => $row["Time_To"],
+                'description' => $row["Description"],
+                'room_type' => $row["room_type"],
+                'module_name' => $row["module_name"],
+                'module_description' => $row["module_description"],
+                'moodle_link' => $row["moodle_link"],
+                'semester' => $row["semester"],
+                'programme_name' => $row["programme_name"],
+                'start_date' => $row["start_date"],
+                'end_date' => $row["end_date"],
+            );
+            $i += 1;
+        }
+        return $array;
+
+    } else {
+        echo "Error Fetching from database";
     }
     $conn->close();
 }
 
-function fetchProgrammes(){//Fetch all programmes
-  include_once 'conn.php';
-$sql ="SELECT * FROM programmes";
-$result = $conn->query($sql);
-return $result;
+function fetchProgrammes()
+{ //Fetch all programmes
+    include_once 'conn.php';
+    $sql = "SELECT * FROM programmes";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          $array[$i] = array(
+            "id: " . $row["id"],
+            " - Name: " . $row["firstname"] . " " . $row["lastname"]
+          );
+          $i += 1;
+        }
+    } else {
+        echo "0 results";
+    }
+    return $result;
 }
 ?>
