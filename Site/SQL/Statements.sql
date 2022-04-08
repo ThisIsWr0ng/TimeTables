81b8adf98c66c714b8abb34c6b7855d27e2b80d1
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-08 16:35:50 +0100

    First working timetable finder

--- Site/SQL/Statements.sql
+++ Site/SQL/Statements.sql
@@ -60,3 +60,25 @@ FROM `events`
 	LEFT JOIN `lecturers_assignment` ON `modules`.`Id` = `lecturers_assignment`.`module`
 	LEFT JOIN `Users` ON `users`.`Id` = `lecturers_assignment`.`lecturer`
 WHERE `student_enrollment`.`Student` = "S19005373";
+
+--Select modules by programme
+SELECT `events`.*,
+	`rooms`.`type` AS room_type,
+	`modules`.`name` AS module_name,
+	`modules`.`description` AS module_description,
+	`modules`.`moodle_link`,
+	`users`.`surname`+', '+`users`.`first_name` AS lecturer,
+	`module_assignment`.`semester`,
+	`programmes`.`name` AS programme_name,
+	`semesters`.`start_date`,
+	`semesters`.`end_date`
+FROM `events`
+	LEFT JOIN `modules` ON `events`.`Module` = `modules`.`Id`
+	LEFT JOIN `module_assignment` ON `module_assignment`.`Module` = `modules`.`Id`
+	LEFT JOIN `programmes` ON `module_assignment`.`Programme` = `programmes`.`Id`
+	LEFT JOIN `semesters` ON `module_assignment`.`Semester` = `semesters`.`Id`
+	LEFT JOIN `rooms` ON `events`.`Room` = `rooms`.`Number`
+	LEFT JOIN `student_enrollment` ON `programmes`.`Id` = `student_enrollment`.`programme`
+	LEFT JOIN `lecturers_assignment` ON `modules`.`Id` = `lecturers_assignment`.`module`
+	LEFT JOIN `Users` ON `users`.`Id` = `lecturers_assignment`.`lecturer`
+WHERE `programmes`.`Id` = "1";
--- Site/timetable.php
+++ Site/timetable.php
@@ -1,1 +1,96 @@
 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title id="page-title">Timetable</title>
+    <meta name="description" content="Personal Timetable" />
+    <link rel="stylesheet" href="Style/Basic.css" />
+    <link href="style/calendar.css" rel="stylesheet" type="text/css" />
+    <script src="script/calendar.js"></script>
+    <?php  include 'php/conn.php'; ?>
+    <?php  include 'php/fetch_data.php'; ?>
+  </head>
+  <body>
+    <header>
+      <img
+        id="logo"
+        src="img/TimeTables-logos/TimeTables-logos_white_cropped.png"
+        alt="TimeTables Logo"
+      />
+    </header>
+    <main class="content">
+
+        <h2 id="demo">Demo</h2>
+      <div id="cal-wrap">
+        <!-- (A) PERIOD SELECTOR -->
+        <div id="cal-date">
+          <select id="cal-mth"></select>
+          <select id="cal-yr"></select>
+        </div>
+
+        <!-- (B) CALENDAR -->
+        <div id="cal-container"></div>
+
+        <!-- (C) EVENT FORM -->
+        <div id="overlay">
+          <form id="cal-event" action="php/eventRequest.php" method="post">
+            <div id="evt-head"></div>
+            <input type="date" id="evt-date" name="date" />
+            <div id="evt-time"></div>
+            <div id="evt-room"></div>
+            <div id="request-fileds">
+              <label for="eName">Event name:</label>
+              <input type="text" id="eName" name="eName" required /><br />
+
+              <label for="tFrom">Time From:</label>
+              <input
+                type="time"
+                id="tFrom"
+                name="timeFrom"
+                min="12:00"
+                max="00:00"
+                required
+              />
+              <label for="tTo">Time To:</label>
+              <input
+                type="time"
+                id="tTo"
+                name="timeTo"
+                min="12:00"
+                max="00:00"
+              /><br />
+
+              <label for="evt-details">Event description:</label>
+            </div>
+
+            <textarea id="evt-details" name="details" readonly></textarea>
+            <input id="evt-close" type="button" value="Close" />
+            <input id="evt-request" type="button" value="Add Event" />
+            <input id="evt-moodle-page" type="button" value="Moodle Page" />
+            <input id="evt-next" type="button" value="Next -->" />
+          </form>
+        </div>
+      </div>
+
+      <script>
+console.log( "headers:",<?php echo json_encode($_REQUEST['type']); ?>, <?php echo json_encode($_REQUEST['id']); ?>);
+
+        function loadContent(a, b){
+            document.getElementById(a).innerHTML= b;
+        }
+            var dbData = JSON.parse( '<?php echo json_encode(fetchProgrammeEvents($_REQUEST['id']))?>' );//Request data
+            var name = "";
+            if(<?php echo json_encode($_REQUEST['type']); ?> == "Programme"){//timetable header
+                name = dbData[0].programme_name;
+            }
+            else if(<?php echo json_encode($_REQUEST['type']); ?> == "Module"){
+                name = dbData[0].module_name;
+            }
+            document.getElementById("demo").innerHTML = `<?php echo json_encode($_REQUEST['type']); ?>: ${name}`;
+            console.log("Data Received:", dbData);
+            window.addEventListener("load", drawCalendar(dbData));
+      </script>
+    </main>
+  </body>
+</html>
