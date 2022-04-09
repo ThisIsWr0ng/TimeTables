2f68b9f84ce5dcd43b8596a8c68b01c523fad9f0
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-09 15:59:26 +0100

    Delete timetable_finder.js

--- Site/script/timetable_finder.js
+++
@@ -1,7 +0,0 @@
-function finderResults(dbData){
-
-    var tResults = document.getElementById('results');
-    var sType =  document.getElementById('search_type');
-    var sInput =  document.getElementById('search_input');
-    var sButton =  document.getElementById('search_button');
-}
