1afe38b182450a6807cf557d08685bd1f1cb9ea4
shahhub <alistairshah2000@gmail.com>
2022-04-11 20:32:49 +0100

    fix form

---
+++ Site/form_users.php
@@ -0,0 +1,109 @@
+
+<html>
+<head>
+<link rel="stylesheet" href="Form.css">
+
+
+
+</head>
+
+<body>
+
+<form id="box">
+<h2>Users</h2>
+
+
+
+<form action="">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br>
+
+  <label for="title">Title:</label><br>
+  <input type="text" id="title" name="title" value=""><br>
+  
+  
+  <form>
+  <label>Gender:</label><br>
+  <select name="gender" id="gender"><br>
+  <optgroup label="Gender">
+	<option value="male">Male</option>
+	<option value="female">Female</option>
+	<option value="transgender">Transgender</option><br>
+  </optgroup>
+  </select>
+  <br>
+  
+  <label for="nok">Next of Kin:</label><br> 
+  <input type="text" id="nok" name="nok" value=""><br>
+  
+  <label for="nok">Private Email:</label><br> 
+  <input type="text" id="pe" name="pe" value=""><br>
+  
+  <br>
+  <br>
+  
+  <label for="sno">Street number:</label><br> 
+  <input type="text" id="sno" name="sno" value=""><br>
+  
+  <label for="sn">Street name:</label><br> 
+  <input type="text" id="sn" name="sn" value=""><br>
+  
+  <label for="pc">Postcode:</label><br> 
+  <input type="text" id="pc" name="pc" value=""><br>
+  
+    
+  <br>
+  <br>
+  
+  <label for="UID">User ID:</label><br> 
+  <input type="text" id="UID" name="UID" value=""><br>
+  
+  <label>Role:</label><br>
+  <select name="Role" id="role"><br>
+  <optgroup label="Role">
+	<option value="Student">Student</option>
+	<option value="Admin">Admin</option>
+  </optgroup>#
+  </select><br> 
+  
+  <label for="UID">User ID:</label><br> 
+  <input type="text" id="UID" name="UID" value=""><br>
+  
+  <label for="pro">Programme:</label><br> 
+  <input type="text" id="pro" name="pro" value=""><br>
+  
+  <label for="lvl">Level:</label><br> 
+  <input type="text" id="lvl" name="lvl" value=""><br>
+  
+  <label for="ue">University Email:</label><br> 
+  <input type="text" id="ue" name="sn" value=""><br>
+  
+  <label for="dpt">Department:</label><br> 
+  <input type="text" id="dpt" name="dpt" value=""><br>
+  
+  <label for="mod">Module:</label><br> 
+  <input type="text" id="mod" name="mod" value=""><br>
+  
+  <br>
+  
+  <button type="button" onclick="">Add</button> <button type="button" onclick="">Delete</button> <button type="button" onclick="">Update</button> <button type="button" onclick="">Search</button> <button type="button" onclick="">Import</button> <button type="button" onclick="">Export</button>
+  
+</form> 
+
+
+
+
+
+
+</form>
+<form id="box2">
+</form>
+
+
+
+
+
+</html>
