7128e67209da022ee4c2f3653152c89ab3206dcd
shahhub <alistairshah2000@gmail.com>
2022-04-11 14:06:27 +0100

    Update personal_timetable.php

--- Site/personal_timetable.php
+++ Site/personal_timetable.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+
+$username = $_SESSION["username"];
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
