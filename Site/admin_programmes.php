02cd58187fcefb0da1b087489dcd2a6534d73ae4
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-13 23:17:12 +0100

    Update admin_programmes.php

--- Site/admin_programmes.php
+++ Site/admin_programmes.php
@@ -1,150 +1,149 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: index.php");
+if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
+    header( "location: index.php" );
     exit;
 }
 
-$username = $_SESSION["username"];
+$username = $_SESSION[ "username" ];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
+<head>
+    <meta charset="utf-8"/>
     <title>Manage Programmes</title>
-    <meta name="description" content="Personal Timetable" />
-    <link rel="stylesheet" href="Style/Basic.css" />
-    <link rel="stylesheet" href="Style/admin.css" />
+    <meta name="description" content="Personal Timetable"/>
+    <link rel="stylesheet" href="Style/Basic.css"/>
+    <link rel="stylesheet" href="Style/admin.css"/>
     <script src="script/searchBox.js"></script>
     <script src="script/sortTable.js"></script>
     <script src="script/searchBox.js"></script>
     <?php include 'php/fetch_data.php'?>
-  </head>
-  <body>
+</head>
+
+<body>
     <header>
-      <img
-        id="logo"
-        src="img/TimeTables-logos/TimeTables-logos_white_cropped.png"
-        alt="TimeTables Logo"
-      />
+        <img id="logo" src="img/TimeTables-logos/TimeTables-logos_white_cropped.png" alt="TimeTables Logo"/>
     </header>
     <input type="button" id="logout" value="Logout" onclick="location.href='php/logout.php'"/>
     <main class="admin-content">
-      <h1 id="admin-title">Manage Programmes</h1>
-      <nav id="admin-nav">
-      <input type="button" value="Home" onclick="location.href='admin_portal.php'"/>
-        <input type="button" value="Users" onclick="location.href='admin_users.php'"/>
-        <input type="button" value="Programmes" onclick="location.href='admin_programmes.php'"/>
-        <input type="button" value="Modules" onclick="location.href='admin_modules.php'"/>
-        <input type="button" value="Events" onclick="location.href='admin_events.php'"/>
-        <input type="button" value="Requests" onclick="location.href='admin_requests.php'"/>
-        <input type="button" value="View Calendar" onclick="location.href='admin_calendar.php'"/>
-      </nav>
-      <div id="content-block">
-      <div id="form-section">
-        <form>
-          <fieldset>
-            <legend>Details</legend>
-        <label for="id">id:</label><br>
-  <input type="text" id="id" name="id" value=""><br>
-      <label for="name">Name:</label><br>
-  <input type="text" id="name" name="name" value="" disabled="true"><br>
+        <h1 id="admin-title">Manage Programmes</h1>
+        <nav id="admin-nav">
+            <input type="button" value="Home" onclick="location.href='admin_portal.php'"/>
+            <input type="button" value="Users" onclick="location.href='admin_users.php'"/>
+            <input type="button" value="Programmes" onclick="location.href='admin_programmes.php'"/>
+            <input type="button" value="Modules" onclick="location.href='admin_modules.php'"/>
+            <input type="button" value="Events" onclick="location.href='admin_events.php'"/>
+            <input type="button" value="Requests" onclick="location.href='admin_requests.php'"/>
+            <input type="button" value="View Calendar" onclick="location.href='admin_calendar.php'"/>
+        </nav>
+        <div id="content-block">
+            <div id="form-section">
+                <form>
+                    <fieldset>
+                        <legend>Details</legend>
+                        <label for="id">id:</label><br>
+                        <input type="text" id="id" name="id" value=""><br>
+                        <label for="name">Name:</label><br>
+                        <input type="text" id="name" name="name" value="" disabled><br>
 
-         <label for="degree">Degree:</label><br>
-            <select id="form-prog-deg" name="degree">
-              <option value="BA (Hons)">BA (Hons)</option>
-              <option value="BA">BA</option>
-              <option value="BEng (Hons)">BEng (Hons)</option>
-              <option value="BSc (Hons)">BSc (Hons)</option>
-              <option value="BSc (Ord)">BSc (Ord)</option>
-              <option value="BSc">BSc</option>
-              <option value="CertEd">CertEd</option>
-              <option value="DipHE">DipHE</option>
-              <option value="FdA">FdA</option>
-              <option value="FdEng">FdEng</option>
-              <option value="FdSc">FdSc</option>
-              <option value="MA">MA</option>
-              <option value="MBA">MBA</option>
-              <option value="MDes">MDes</option>
-              <option value="MRes">MRes</option>
-              <option value="MSc">MSc</option>
-              <option value="PGCE">PGCE</option>
-            </select>
+                        <label for="degree">Degree:</label><br>
+                        <select id="form-prog-deg" name="degree">
+                            <option value="BA (Hons)">BA (Hons)</option>
+                            <option value="BA">BA</option>
+                            <option value="BEng (Hons)">BEng (Hons)</option>
+                            <option value="BSc (Hons)">BSc (Hons)</option>
+                            <option value="BSc (Ord)">BSc (Ord)</option>
+                            <option value="BSc">BSc</option>
+                            <option value="CertEd">CertEd</option>
+                            <option value="DipHE">DipHE</option>
+                            <option value="FdA">FdA</option>
+                            <option value="FdEng">FdEng</option>
+                            <option value="FdSc">FdSc</option>
+                            <option value="MA">MA</option>
+                            <option value="MBA">MBA</option>
+                            <option value="MDes">MDes</option>
+                            <option value="MRes">MRes</option>
+                            <option value="MSc">MSc</option>
+                            <option value="PGCE">PGCE</option>
+                        </select>
 
-  <label for="department">Department:</label><br>
-            <select id="form-prog-dept" name="department">
-              <?php
-require_once "php/conn.php";
+                        <label for="department">Department:</label><br>
+                        <select id="form-prog-dept" name="department">
+                            <?php
+                            require_once "php/conn.php";
 
-$sql = "SELECT * FROM Departments";
-$result = $conn->query($sql);
-while ($row = mysqli_fetch_array($result)) {
-    $name = $row['Name'];
-    echo "<option value='$name'>$name</option>";
-}
-?>
-            </select>
+                            $sql = "SELECT * FROM Departments";
+                            $result = $conn->query( $sql );
+                            while ( $row = mysqli_fetch_array( $result ) ) {
+                                $name = $row[ 'Name' ];
+                                echo "<option value='$name'>$name</option>";
+                            }
+                            ?>
+                        </select>
 
-            <label>Level</label><br>
-            <select id="form-prog-level" name="level">
-            <option value="3">3</option>
-            <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-            </select>
-            <label>Start Date</label><br>
-          <input id="form-prog-sdate" type="date" name="dob"><br><br>
+                        <label>Level</label><br>
+                        <select id="form-prog-level" name="level">
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                        </select>
+                        <label>Start Date</label><br>
+                        <input id="form-prog-sdate" type="date" name="dob"><br><br>
 
-          <label>End Date</label><br>
-          <input id="form-prog-edate" type="date" name="dob"><br><br>
+                        <label>End Date</label><br>
+                        <input id="form-prog-edate" type="date" name="dob"><br><br>
 
-  <label for="desc">Description</label><br>
-  <textarea id="form-prog-descc" name="desc" rows="4" cols="50">
-  </textarea> <br></fieldset>
-</form>
-<form action="">
-  <fieldset>
-    <legend>Modules</legend>
-            <select id="form-prog-sem" name="semester">
-              <?php
-require_once "php/conn.php";
+                        <label for="desc">Description</label><br>
+                        <textarea id="form-prog-descc" name="desc" rows="4" cols="50">
+  </textarea> <br>
+                    </fieldset>
+                </form>
+                <form action="">
+                    <fieldset>
+                        <legend>Modules</legend>
+                        <select id="form-prog-sem" name="semester">
+                            <?php
+                            require_once "php/conn.php";
 
-$sql = "SELECT * FROM Semesters";
-$result = $conn->query($sql);
-while ($row = mysqli_fetch_array($result)) {
-    $name = $row['Name'];
-    $year = $row['Year'];
-    echo "<option value='$name'>$name, $year</option>";
-}
-?>
-<section id="form-prog-modules"></section>
-  </fieldset>
-</form>
-     </div></div>
-     <div id="search-section">
-     <form class="admin-search">
-         <select name="search-type" id="search-type">
-           <option value="Users" selected>Users</option>
-           <option value="Programmes">Programmes</option>
-           <option value="Modules">Modules</option>
-           <option value="Events">Events</option>
-         </select>
-         <input type="text" id="search-searchbar"onkeyup="searchBar(this.value)" value="Search" onclick='removeText()' tabindex='1'/>
-       </form>
-       <div id="search-list-opt-top">
-       <input type="button" name="list-remove" id="list-remove" value="-">
-       <input type="button" name="list-add" id="list-add" value="+" onclick=""></div>
-       <div id="search-output"></div>
-       <div id="search-list-opt">
-         <input type="button"value="Import List">
-         <input type="button"value="Export list">
-       </div>
-     </div>
-</div>
-     </div>
+                            $sql = "SELECT * FROM Semesters";
+                            $result = $conn->query( $sql );
+                            while ( $row = mysqli_fetch_array( $result ) ) {
+                                $name = $row[ 'Name' ];
+                                $year = $row[ 'Year' ];
+                                echo "<option value='$name'>$name, $year</option>";
+                            }
+                            ?></select>
+                            <section id="form-prog-modules"></section>
+                    </fieldset>
+                </form>
+            </div>
+            <div id="search-section">
+                <form class="admin-search">
+                    <select name="search-type" id="search-type">
+                        <option value="Users" selected>Users</option>
+                        <option value="Programmes">Programmes</option>
+                        <option value="Modules">Modules</option>
+                        <option value="Events">Events</option>
+                    </select>
+                    <input type="text" id="search-searchbar" onkeyup="searchBar(this.value)" value="Search" onclick='removeText()' tabindex='1'/>
+                </form>
+                <div id="search-list-opt-top">
+                    <input type="button" name="list-remove" id="list-remove" value="-">
+                    <input type="button" name="list-add" id="list-add" value="+" onclick="">
+                </div>
+                <div id="search-output"></div>
+                <div id="search-list-opt">
+                    <input type="button" value="Import List">
+                    <input type="button" value="Export list">
+                </div>
+            </div>
+        </div>
+        </div>
     </main>
-  </body>
+</body>
 </html>
