fbbfa5f74d35553c91faa233b5b984ec62bdbf64
shahhub <alistairshah2000@gmail.com>
2022-04-20 17:39:18 +0100

    minor fix for the php

--- Site/admin_users.php
+++ Site/admin_users.php
@@ -44,7 +44,7 @@ $username = $_SESSION["username"];
       <div id="content-block">
       <div id="form-section">
       <section>
-        <form id="users-form"action="php/addUser.php" method="POST">
+        <form id="users-form"action="php/user.php" method="POST">
           <h1>Users</h1>
           <section>
             <fieldset>
