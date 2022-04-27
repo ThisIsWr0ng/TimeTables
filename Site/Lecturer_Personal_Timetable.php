f7654c039d947a6fa7fa63d487bf009c71b79d1a
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-04-27 14:38:28 +0100

    Create Lecturer_Personal_Timetable.php

---
+++ Site/Lecturer_Personal_Timetable.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+
+$username = $_SESSION["username"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Personal Timetable</title>
+    <meta name="description" content="Personal Timetable" />
+    <link rel="stylesheet" href="Style/Basic.css" />
+    <link href="style/calendar.css" rel="stylesheet" type="text/css" />
+    <script src="script/calendar.js"></script>
+    <?php  include 'php/conn.php'; ?>
+    <?php  include 'php/fetch_data.php'; ?>
+  </head>
+  <body>
+    <header>
+      <img
+        id="logo"
+        src="img/TimeTables-logos/TimeTables-logos_white_cropped.png"
+        alt="TimeTables Logo"
+      />
+    </header>
+    <input type="button" id="logout" value="Logout" onclick="location.href='php/logout.php'"/>
+    <?php echo "<h3 id='log'>Logged in: $username</h3>" ?>
+    <input type="button" value="Timetable Finder" onclick="location.href='timetable_finder.php'"/>
+    <main class="content">
+      <div id="cal-wrap">
+        <!-- (A) PERIOD SELECTOR -->
+        <div id="cal-date">
+          <select id="cal-mth"></select>
+          <select id="cal-yr"></select>
+        </div>
+
+        <!-- (B) CALENDAR -->
+        <div id="cal-container"></div>
+
+        <!-- (C) EVENT FORM -->
+        <div id="overlay">
+          <form id="cal-event" action="php/eventRequest.php" method="post">
+            <div id="evt-head"></div>
+            <input type="date" id="evt-date" name="date" />
+            <div id="evt-time"></div>
+            <div id="evt-room"></div>
+            <div id="request-fileds">
+              <label for="eName">Event name:</label>
+              <input type="text" id="eName" name="eName" required /><br />
+
+              <label for="tFrom">Time From:</label>
+              <input
+                type="time"
+                id="tFrom"
+                name="timeFrom"
+                min="12:00"
+                max="00:00"
+                required
+              />
+              <label for="tTo">Time To:</label>
+              <input
+                type="time"
+                id="tTo"
+                name="timeTo"
+                min="12:00"
+                max="00:00"
+              /><br />
+
+              <label for="evt-details">Event description:</label>
+            </div>
+
+            <textarea id="evt-details" name="details" readonly></textarea>
+            <input id="evt-close" type="button" value="Close" />
+            <input id="evt-request" type="button" value="Add Event" />
+            <input id="evt-moodle-page" type="button" value="Moodle Page" />
+            <input id="evt-next" type="button" value="Next -->" />
+          </form>
+        </div>
+      </div>
+
+      <script>
+        function loadContent(a, b){
+            document.getElementById(a).innerHTML= b;
+        
+        }
+            var dbData = JSON.parse( '<?php echo json_encode(fetchEvents($username)) ?>' );
+            console.log("Data Received:", dbData);
+            window.addEventListener("load", drawCalendar(dbData));
+      </script>
+    </main>
+  </body>
+</html>
