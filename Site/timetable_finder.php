5c96ba1f0387cbc895e9ee3aefa44effc9b01588
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-05-09 09:49:02 +0100

    responsive design

--- Site/timetable_finder.php
+++ Site/timetable_finder.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <title>Timetable Finder</title>
-    <meta name="description" content="Personal Timetable" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Timetable Finder" />
     <link rel="stylesheet" href="Style/Basic.css" />
     <script src="script/sortTable.js"></script>
   </head>
